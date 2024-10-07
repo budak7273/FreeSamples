2c293ee1c6b3467dfd33d6158bb30684b10e731f
Jarno Westhof <jarnowesthof@gmail.com>
2024-10-07 13:02:32 +0200

    Well i broke the BP side, so i fixed it a less breaking way now

--- Source/FreeSamples/Private/Subsystem/FreeSamplesSubsystem.cpp
+++ Source/FreeSamples/Private/Subsystem/FreeSamplesSubsystem.cpp
@@ -1,16 +1,13 @@
 #include "Subsystem/FreeSamplesSubsystem.h"
+#include "kismet/GameplayStatics.h"
 
 bool AFreeSamplesSubsystem::PlayerClaimSamples_Implementation(bool manuallyTriggered, APlayerState* player, bool dropExcessOnGround) {
 	UE_LOG(LogFreeSamplesCpp, Error, TEXT("No C++ implementation yet for PlayerClaimSamples_Implementation, make sure this is overridden in Blueprint"));
 	return false;
 }
 
-AFreeSamplesSubsystem* AFreeSamplesSubsystem::Get(UWorld* world) {
-	USubsystemActorManager* SubsystemActorManager = world->GetSubsystem<USubsystemActorManager>();
-	fgcheck(SubsystemActorManager);
-
-	AFreeSamplesSubsystem* freeSamplesSubsystem = SubsystemActorManager->GetSubsystemActor<AFreeSamplesSubsystem>();
-	fgcheck(freeSamplesSubsystem);
-
-	return freeSamplesSubsystem;
+AFreeSamplesSubsystem* AFreeSamplesSubsystem::Get(UObject* WorldContext) {
+	// This approach would break if a C++ version was spawned or was persisted via save game
+	// The blueprint version descends from this class, so it gets found instead as long as it got spawned properly
+	return Cast<AFreeSamplesSubsystem>(UGameplayStatics::GetActorOfClass(WorldContext, StaticClass()));
 }

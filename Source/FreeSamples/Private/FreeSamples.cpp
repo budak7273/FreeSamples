01bcf00ecf5eca2de2a1229b79d3cdd415d0340d
Rob B <computerguy440+gh@gmail.com>
2025-06-09 12:13:20 -0400

    Clean up old custom hooking code. Add Mercer Sphere and Somersloop to skip list.

--- Source/FreeSamples/Private/FreeSamples.cpp
+++ Source/FreeSamples/Private/FreeSamples.cpp
@@ -7,50 +7,9 @@
 
 DEFINE_LOG_CATEGORY(LogFreeSamplesCpp);
 
-void OnFgCopyProperties(CallScope<void(*)(AFGPlayerState*, APlayerState*)>& scope, AFGPlayerState* self, APlayerState* playerState) {
-	UE_LOG(LogFreeSamplesCpp, VeryVerbose, TEXT("OnFgCopyProperties hook"));
-	if (auto component = self->GetComponentByClass<UFreeSamplePlayerDataComponent>()) {
-		component->CopyProperties(playerState);
-	} else {
-		UE_LOG(LogFreeSamplesCpp, Warning, TEXT("OnFgCopyProperties PlayerState had no UFreeSamplePlayerDataComponent"));
-	}
-
-	scope(self, playerState);
-}
-
-//void OnCopyProperties(CallScope<void(*)(APlayerState*, APlayerState*)>& scope, APlayerState* self, APlayerState* playerState) {
-//	UE_LOG(LogFreeSamplesCpp, VeryVerbose, TEXT("CopyProperties hook"));
-//	scope(self, playerState);
-//}
-
-void OnDispatchOverrideWith(CallScope<void(*)(APlayerState*, APlayerState*)>& scope, APlayerState* self, APlayerState* playerState) {
-	UE_LOG(LogFreeSamplesCpp, VeryVerbose, TEXT("OnDispatchOverrideWith hook"));
-	if (auto component = self->GetComponentByClass<UFreeSamplePlayerDataComponent>()) {
-		component->OverrideWith(playerState);
-	} else {
-		UE_LOG(LogFreeSamplesCpp, Warning, TEXT("OnDispatchOverrideWith PlayerState had no UFreeSamplePlayerDataComponent"));
-	}
-
-	scope(self, playerState);
-}
-
-void ApplyPlayerStateHooks() {
-	// TODO once SF 1.1 is released there will be a nice bp-accessible entry point for this and we won't have to hook it from c++
-
-	//AFGPlayerState* SampleObject1 = GetMutableDefault<AFGPlayerState>();
-	//SUBSCRIBE_METHOD_VIRTUAL(AFGPlayerState::CopyProperties, SampleObject1, &OnFgCopyProperties);
-
-	//APlayerState* SampleObject2 = GetMutableDefault<APlayerState>();
-	////SUBSCRIBE_METHOD_VIRTUAL(APlayerState::DispatchCopyProperties, SampleObject2, &OnCopyProperties);
-	//SUBSCRIBE_METHOD_VIRTUAL(APlayerState::DispatchOverrideWith, SampleObject2, &OnDispatchOverrideWith);
-}
-
 void FFreeSamplesModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-#if !WITH_EDITOR
-	ApplyPlayerStateHooks();
-#endif
 }
 
 void FFreeSamplesModule::ShutdownModule()
--- Source/FreeSamples/Public/Data/FreeSamplePlayerDataComponent.h
+++ Source/FreeSamples/Public/Data/FreeSamplePlayerDataComponent.h
@@ -7,19 +7,10 @@
 #include "FreeSamplePlayerDataComponent.generated.h"
 
 /**
-  * C++ parent class so the hook has a class to look up via. All implementation in blueprint.
+  * C++ parent class, previously so the hook has a class to look up via, now not really needed. All implementation in blueprint.
 */
 UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class FREESAMPLES_API UFreeSamplePlayerDataComponent : public UActorComponent
 {
 	GENERATED_BODY()
-
-public:
-	// Copy properties which need to be saved from this component to the passed inactive PlayerState
-	UFUNCTION(BlueprintImplementableEvent)
-	void CopyProperties(APlayerState* NewPlayerState);
-
-	// Set properties on this component from the passed inactive PlayerState to restore from
-	UFUNCTION(BlueprintImplementableEvent)
-	void OverrideWith(APlayerState* OldPlayerState);
 };
